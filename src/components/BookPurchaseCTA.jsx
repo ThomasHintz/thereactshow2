1d0dede7ff871b142187b183c08b43fa8bb5624a
Thomas Hintz <t@thintz.com>
2023-01-30 11:43:17 -0800

    Using real stripe link.

--- src/components/BookPurchaseCTA.jsx
+++ src/components/BookPurchaseCTA.jsx
@@ -5,8 +5,7 @@ export default function BookPurchaseCTA() {
     <>
       <CenteredDarkPanel title="Foundations of High-Performance React" buttonText="Purchase"
                          id="first-cta"
-                         href2="https://buy.stripe.com/bIY6rpgbG3d2ces000"
-                         href="https://buy.stripe.com/test_3cs7tL9egc214JG288"
+                         href="https://buy.stripe.com/bIY6rpgbG3d2ces000"
       >
         <p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-300">
           $11.99
