787896bbc9263063e3e63dadd52c3291a18cb10b
Thomas Hintz <t@thintz.com>
2023-01-30 11:42:50 -0800

    Fixing styles for book downloads.

--- src/app/(extra)/book-success/page.jsx
+++ src/app/(extra)/book-success/page.jsx
@@ -8,21 +8,21 @@ export default async function Page({ params }) {
           <a
             download
             href="/files/foundations-high-performance-react.pdf"
-            className="rounded-md bg-white px-3.5 py-1.5 text-base font-semibold leading-7 text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+            className="rounded-md bg-gray-800 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray"
           >
             Download PDF
           </a>
           <a
             href="/files/foundations-high-performance-react.epub"
             download
-            className="rounded-md bg-white px-3.5 py-1.5 text-base font-semibold leading-7 text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+            className="rounded-md bg-gray-800 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray"
           >
             Download ePub
           </a>
           <a
             href="/files/foundations-high-performance-react.mobi"
             download
-            className="rounded-md bg-white px-3.5 py-1.5 text-base font-semibold leading-7 text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+            className="rounded-md bg-gray-800 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray"
           >
             Download Kindle
           </a>
