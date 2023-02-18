7d176c3817a596ea304926564af44ed347bdea57
Thomas Hintz <t@thintz.com>
2023-02-18 09:37:39 -0800

    Adding YouTube link.

--- src/app/(main)/layout.jsx
+++ src/app/(main)/layout.jsx
@@ -126,6 +126,18 @@ function RSSIcon(props) {
   )
 }
 
+function YouTubeIcon(props) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 48 48" width="32px" height="32px" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M43.2,33.9c-0.4,2.1-2.1,3.7-4.2,4c-3.3,0.5-8.8,1.1-15,1.1c-6.1,0-11.6-0.6-15-1.1c-2.1-0.3-3.8-1.9-4.2-4C4.4,31.6,4,28.2,4,24c0-4.2,0.4-7.6,0.8-9.9c0.4-2.1,2.1-3.7,4.2-4C12.3,9.6,17.8,9,24,9c6.2,0,11.6,0.6,15,1.1c2.1,0.3,3.8,1.9,4.2,4c0.4,2.3,0.9,5.7,0.9,9.9C44,28.2,43.6,31.6,43.2,33.9z"/>
+      <path fill="#FFF" d="M20 31L20 17 32 24z"/>
+    </svg>
+  );
+};
+
 function PersonIcon(props) {
   return (
     <svg aria-hidden="true" viewBox="0 0 11 12" {...props}>
@@ -222,7 +234,7 @@ function Layout({ children }) {
                 {[
                    ['Spotify', SpotifyIcon, 'https://open.spotify.com/show/7tqGIm6Y1g5idqU87TcpJv'],
                    ['Apple Podcast', ApplePodcastIcon, 'https://podcasts.apple.com/podcast/id1566939019'],
-                   ['Overcast', OvercastIcon, 'https://overcast.fm/itunes1566939019'],
+                   ['YouTube', YouTubeIcon, 'https://www.youtube.com/channel/UCKbaf175mWnjSRt9duBPKbQ'],
                    ['RSS Feed', RSSIcon, 'https://feeds.buzzsprout.com/1764837.rss'],
                 ].map(([label, Icon, url]) => (
                   <li key={label} className="flex">
