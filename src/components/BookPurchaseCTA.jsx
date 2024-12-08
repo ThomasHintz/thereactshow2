b9db644b3f52ef212b3f19447463b62f5daa5a44
Thomas Hintz <t@thintz.com>
2024-12-07 19:00:12 -0800

    Update BookPurchaseCTA.jsx

--- src/components/BookPurchaseCTA.jsx
+++ src/components/BookPurchaseCTA.jsx
@@ -3,12 +3,12 @@ import CenteredDarkPanel from '@/components/CenteredDarkPanel'
 export default function BookPurchaseCTA() {
   return (
     <>
-      <CenteredDarkPanel title="Foundations of High-Performance React" buttonText="Purchase"
+      <CenteredDarkPanel title="Foundations of High-Performance React" buttonText="Download"
                          id="first-cta"
                          href="https://buy.stripe.com/14kcPNaRm28Ydiw7su"
       >
         <p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-300">
-          $11.99 suggested (min $0.50)
+          Now Available For Free!
         </p>
         <p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-300">
           DRM-free Kindle, ePub, & PDF downloads.
